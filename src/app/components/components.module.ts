9a454dfa0600a19715b524ec9e1e1d58c2057042
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-11-22 16:02:40 -0500

    Page components move to pages folder

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuToggleComponent } from './shared/menu-toggle/menu-toggle.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
-import { VideoGamesComponent } from './video-games/video-games.component';
+import { VideoGamesComponent } from './pages/video-games/video-games.component';
 
 
 
